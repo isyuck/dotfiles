c897ba7daf2c6a95d1b2d2fe84c50b7dd95bcd28
isaac <isaac@isaac.ac>
2022-03-22 15:56:45 +0000

    enable C# lang support

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -117,7 +117,7 @@
        ;;common-lisp       ; if you've seen one lisp, you've seen them all
        ;;coq               ; proofs-as-programs
        ;;crystal           ; ruby at the speed of c
-       ;;csharp            ; unity, .NET, and mono shenanigans
+       csharp            ; unity, .NET, and mono shenanigans
        data              ; config/data formats
        ;;(dart +flutter)   ; paint ui and not much else
        ;;elixir            ; erlang done right
